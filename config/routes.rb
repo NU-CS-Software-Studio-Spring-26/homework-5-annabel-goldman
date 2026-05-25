b82f72754fe36b738cc448e9b405e3409ad45598
AnnabelGoldman <annabel@crossplaydigital.com>
2026-05-25 12:39:03 -0500

    Fix RuboCop string literal style in routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
       get :week_old
     end
   end
-  get '/hello', to: 'todos#hello'
+  get "/hello", to: "todos#hello"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
